cb62c81e38ab4ead0b03669b6aade8eef73a2f3f
majdba123 <137396563+majdba123@users.noreply.github.com>
2025-04-29 15:10:51 +0300

    1
    1

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+// config.js
+window.API_CONFIG = {
+    BASE_URL: 'https://highleveltecknology.com/Sell-Your-Device-back/backend', 
+    ENDPOINTS: {
+        LOGIN: '/api/login', 
+    },
+    DEFAULT_HEADERS: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+};
